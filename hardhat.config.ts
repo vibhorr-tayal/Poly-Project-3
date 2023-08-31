36f6cddb04a8665aa7ea049984218063b6d24a15
Vibhor Tayal <reachvibhortayal@gmail.com>
2023-09-01 02:40:49 +0530

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,43 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+// https://github.com/projectsophon/hardhat-circom
+import "hardhat-circom";
+// circuits
+import circuits = require('./circuits.config.json')
+
+// set env var to the root of the project
+process.env.BASE_PATH = __dirname;
+
+// tasks
+import "./tasks/newcircuit.ts"
+
+const config: HardhatUserConfig = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.17",
+      },
+      {
+        version: "0.6.11",
+      }
+    ]
+  },
+  circom: {
+    // (optional) Base path for input files, defaults to `./circuits/`
+    inputBasePath: "./circuits",
+    // (required) The final ptau file, relative to inputBasePath, from a Phase 1 ceremony
+    ptau: "powersOfTau28_hez_final_12.ptau",
+    // (required) Each object in this array refers to a separate circuit
+    circuits: JSON.parse(JSON.stringify(circuits))
+  },
+  networks: {
+    goerli: {
+      url: "https://rpc.ankr.com/eth_goerli", // Replace with your Infura Project ID or Goerli node URL
+      accounts: {
+        mnemonic: "easy mistake diagram stage category monster ball oven winter liar mistake mosquito" // Replace with your actual mnemonic phrase
+      }
+    }
+  }
+};
+
+export default config;
